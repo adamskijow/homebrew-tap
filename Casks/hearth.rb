f28dd63355844b8033e239ad2447aa17cdea8ccd
adamskijow <adamskijow@gmail.com>
2026-06-30 08:18:56 -0400

    hearth 0.2.0

--- Casks/hearth.rb
+++ Casks/hearth.rb
@@ -7,8 +7,8 @@
 #   brew install --cask adamskijow/tap/hearth
 # (after `brew tap adamskijow/tap`), or point brew straight at this file.
 cask "hearth" do
-  version "0.1.0"
-  sha256 "26a69761d20631a0b3823a123f0379dd5e2259da94d19ec1fafeda3cbe5ef9c8"
+  version "0.2.0"
+  sha256 "054c3af96d0907f5169b43cce18e8c1b2790f54c19c599f0734715bdbbedbf7f"
 
   url "https://github.com/adamskijow/Hearth/releases/download/v#{version}/Hearth-#{version}.dmg"
   name "Hearth"
